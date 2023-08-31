d28d38309da09169427395c82a8b172cbff6aa42
Artem Nosov <103992767+avnosov3@users.noreply.github.com>
2023-08-31 22:03:02 +0200

    init customuser and opt admin panels

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,1 +1,16 @@
-# from django.contrib import admin
+from django.contrib import admin
+
+from users import models
+
+
+@admin.register(models.CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'username',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(models.OTP)
+class OTPAdmin(admin.ModelAdmin):
+    list_display = ('user', 'code',)
+    list_filter = ('user',)
+    empty_value_display = '-пусто-'
