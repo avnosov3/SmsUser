4871def5d9968428d4a36fa214387b7c56268bec
Artem Nosov <103992767+avnosov3@users.noreply.github.com>
2023-09-01 18:48:50 +0200

    create, change and delete in OPT admin panel is forbidden

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -14,4 +14,11 @@ class CustomUserAdmin(admin.ModelAdmin):
 class OTPAdmin(admin.ModelAdmin):
     list_display = ('user', 'code',)
     list_filter = ('user',)
+    readonly_fields = ('user', 'code', 'expires_at')
     empty_value_display = '-пусто-'
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
