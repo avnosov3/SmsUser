26c16dc9d83ee3a537ed749e464d564ec44b0020
Artem Nosov <103992767+avnosov3@users.noreply.github.com>
2023-09-01 12:22:32 +0200

    customize user model: remove username form user object manager

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,41 +1,69 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 from django.utils import timezone
 
 from users import settings
 
 
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None):
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
 class CustomUser(AbstractUser):
     username = models.CharField(
         verbose_name='Юзернейм',
-        unique=True,
         blank=True,
-        max_length=settings.MAIN_LENGTH
+        max_length=settings.MAIN_LENGTH,
     )
     email = models.EmailField(
         unique=True,
         verbose_name='Электронная почта',
-        max_length=settings.MAIN_LENGTH
-    )
-    first_name = models.CharField(
-        verbose_name='Имя',
-        max_length=settings.MAIN_LENGTH
-    )
-    last_name = models.CharField(
-        verbose_name='Фамилия',
-        max_length=settings.MAIN_LENGTH
+        max_length=settings.MAIN_LENGTH,
     )
     confirmation_code = models.CharField(
-        blank=True,
         verbose_name='Код подтверждения',
-        max_length=settings.MAIN_LENGTH
+        max_length=settings.MAIN_LENGTH,
+        blank=True,
     )
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
+    objects = MyUserManager()
+
     def __str__(self):
         return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
 
     class Meta:
         verbose_name = 'Пользователь'
